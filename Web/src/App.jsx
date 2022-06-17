43500e7a654413c14843165669f501d5af836cc2
tnioa <hortanio@hotmail.com>
2022-06-17 19:53:36 -0300

    Finalizando trabalho

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -12,7 +12,7 @@ const HUMIDITY_LOGS_NAME = "dataPointsHumd";
 const DIGITAL_LIGHT_PIN = 7;
 const VIRTUAL_TEMPERATURE_PIN= 150;
 const VIRTUAL_HUMIDITY_PIN= 151;
-const GET_WETHER_VALUES_INTERVAL = 10000;
+const GET_WETHER_VALUES_INTERVAL = 60000;
 
 const tempData = [{"label":"23:10","y":29.2,"x":0},{"label":"23:10","y":29.2,"x":1},{"label":"23:10","y":28.9,"x":2},{"label":"23:10","y":28.5,"x":3},{"label":"23:10","y":28.2,"x":4},{"label":"23:10","y":27.9,"x":5},{"label":"23:11","y":27.7,"x":6},{"label":"23:11","y":27.5,"x":7},{"label":"23:11","y":27.5,"x":8},{"label":"23:11","y":27.2,"x":9},{"label":"23:11","y":26.9,"x":10},{"label":"23:12","y":26.5,"x":11},{"label":"23:12","y":26.8,"x":12},{"label":"23:12","y":26.9,"x":13},{"label":"23:12","y":26.2,"x":14}]
 const humidData = [{"label":"15:10","y":87,"x":0},{"label":"15:25","y":87,"x":1},{"label":"15:32","y":87,"x":2},{"label":"15:43","y":85,"x":3},{"label":"15:52","y":84,"x":4},{"label":"16:11","y":86,"x":5},{"label":"16:20","y":86,"x":6},{"label":"16:35","y":86,"x":7},{"label":"16:42","y":88,"x":8},{"label":"16:51","y":88,"x":9},{"label":"17:01","y":89,"x":10},{"label":"17:12","y":85,"x":11},{"label":"17:23","y":84,"x":12},{"label":"17:30","y":85,"x":13},{"label":"17:45","y":86,"x":14},{"label":"17:56","y":87}]
@@ -58,10 +58,12 @@ export default class App extends React.Component {
         fetch(`${API_URL}/setpintype?pin=${DIGITAL_LIGHT_PIN}&type=output`).then(x=> x.json()).then(x=> {
             if(!x.success) alert("Erro na configuração da lâmpada.");
         });
+        fetch(`${API_URL}/setdigitalpin?pin=${DIGITAL_LIGHT_PIN}&value=1`).then(x=> x.json())
+            .then(()=> this.setState({lamp: !this.state.lamp}));
     }
 
     setLamp(){
-        fetch(`${API_URL}/setdigitalpin?pin=${DIGITAL_LIGHT_PIN}&value=${!this.state.lamp ? 1 : 0}`).then(x=> x.json())
+        fetch(`${API_URL}/setdigitalpin?pin=${DIGITAL_LIGHT_PIN}&value=${!this.state.lamp ? 0 : 1}`).then(x=> x.json())
             .then(()=> this.setState({lamp: !this.state.lamp}));
     }
 
